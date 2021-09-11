62ffcfab5cf9050e4e39a51fe4922603c46c3cda
Zio <zi.ao.tan@outlook.com>
2021-09-11 18:39:23 +0800

    fetch data from fake api

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,15 +2,16 @@ import { Injectable } from '@angular/core';
 import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks';
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
